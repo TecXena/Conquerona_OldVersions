3fe443bd1fa2bb843a86d426da6cfcbc78ea625e
Elijah <franzvasquez4153@Gmail.com>
2021-03-30 10:59:31 +0800

    Implemented Dialog system, Camera Movement, and world prototype

---
+++ Assets/Trigger_btnBottomRight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Trigger_btnBottomRight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+     public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("T_BottomRight");
+        animator.SetBool("Bool_WalkRight", true);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("F_BottomRight");
+        animator.SetBool("Bool_WalkRight", false);
+    }
+}
