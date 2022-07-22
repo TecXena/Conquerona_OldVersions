d2736b27cbebedb88e482c382f2cbe4eb553afe6
Xena <ariadnetec@gmail.com>
2022-07-22 17:04:23 +0800

    Conquerona 2021-3-3 4d Movement for PC only

---
+++ Assets/btnRight_Walk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class btnRight_Walk : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("TrueRight");
+        animator.SetFloat("Vertical", 1);
+        animator.SetFloat("Horizontal", 0);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("FalseRight");
+        animator.SetFloat("Vertical", 0);
+        animator.SetFloat("Horizontal", 0);
+    }
+
+}
